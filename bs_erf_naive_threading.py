44746ae30e7ae47b294143f6cd44e18a2f8279fc
Frank Schlimbach <frank.schlimbach@intel.com>
2017-03-07 11:30:39 -0600

    fixed typo

--- bs_erf_naive_threading.py
+++ bs_erf_naive_threading.py
@@ -1,5 +1,5 @@
-from bs_erf_naive import black_scholes
+from bs_erf_naive import black_scholes_args
 from bs_erf_threading import main
 
 if __name__ == '__main__':
-    main(__file__, black_scholes)
+    main(__file__, black_scholes_args)
